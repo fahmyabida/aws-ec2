fce3c86a6b04e0ea3f8bf13be0407f7bd1391239
fahmyabida <fahmyabida@gmail.com>
2022-05-13 20:17:55 +0700

    update model

--- model/user.go
+++ model/user.go
@@ -1,11 +1,27 @@
 package model
 
+import "gorm.io/datatypes"
+
 type User struct {
-	Id       int    `gorm:"primaryKey" json:"id"`
-	Username string `json:"username"`
-	Password string `json:"-"`
-	Name     string `json:"name"`
-	Role     string `json:"role"`
+	Id            int    `gorm:"primaryKey" json:"id"`
+	Username      string `json:"username"`
+	Password      string `json:"-"`
+	Name          string `json:"name"`
+	Role          string `json:"role"`
+}
+
+type UserContoh struct {
+	Id            int    `gorm:"primaryKey" json:"id"`
+	Username      string `json:"username"`
+	Password      string `json:"-"`
+	Name          string `json:"name"`
+	Role          string `json:"role"`
+	IdJenisMuatan datatypes.JSON `gorm:column="id_jenis_muatan"`
+}
+
+func CobaInsert() {
+	user := UserContoh{}
+	user.IdJenisMuatan = []byte("[1,2,3,4,5]")
 }
 
 type UserCostum struct { // table user_custom
